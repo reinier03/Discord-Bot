eabd51bd047d594ca0d99f328fa5ea3efe78b984
Reima <reiniermayea@gmail.com>
2024-01-21 02:00:00 -0500

    Memes channel added to the bot :D

--- Discord Bot.py
+++ Discord Bot.py
@@ -1,5 +1,17 @@
 import easy_bot_reima
 import os
+import threading
+import requests 
+from bs4 import BeautifulSoup as bs
+from time import sleep
+
+
+id_canal=1189687855774191687
+
+
+user={"user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:121.0) Gecko/20100101 Firefox/121.0"}
+diccionario={}
+
 
 
 intentos = easy_bot_reima.discord.Intents.all()
@@ -47,7 +59,72 @@ async def on_message(message):
             #await message.channel.send(new_content)
         
 
-# Token de autenticación del bot (debes reemplazar esto con tu propio token)
+
+
+
+
+def obtener_memes():
+    global diccionario
+    global user
+    contador=0
+    diccionario.clear()
+    while not len(diccionario)>=48:
+        contador+=1
+        res=requests.get(f"https://www.memedroid.com/memes/random?page={contador}", headers=user)
+        soup=bs(res.text, features="html.parser")
+        articulos=soup.find_all("article", class_="gallery-item")
+        for e, i in enumerate(articulos, start=len(diccionario)+1):
+            try:
+                imagen=i.find("img", class_="img-responsive grey-background").attrs.get("src")
+                texto=i.find("a", class_="item-header-title dyn-link").text
+                diccionario[e]=[imagen, texto]
+                if len(diccionario)>= 48:
+                    break
+            except:
+                video=i.find("video", class_="item-video gallery-item-video grey-background").find("source").attrs.get("src")
+                texto=i.find("a", class_="item-header-title dyn-link").text
+                diccionario[e]=[video, texto]
+                if len(diccionario)>= 48:
+                    break
+                
+                
+    return publicar(diccionario, user)
+
+
+def publicar(diccionario, user):
+    global id_canal
+    canal=client.get_channel(id_canal) #reemplazar este valor con el chat id del destino
+    for e, i in enumerate(diccionario, start=1):
+        res=requests.get(diccionario[e][0], headers=user)
+        
+        with open(f"{os.path.basename(diccionario[e][0])}", "wb") as archivo_escritura:
+            archivo_escritura.write(res.content)
+            
+        archivo_lectura=open(f"{os.path.basename(diccionario[e][0])}", "rb")
+        archivo=easy_bot_reima.discord.File(archivo_lectura)
+        canal.send(f"{diccionario[e][1]}",file=archivo) 
+                
+        archivo_lectura.close()
+        os.remove(os.path.basename(diccionario[e][0]))
+        sleep(1800)
+    return
+            
+                
+                
+                
+obtener_memes()
+
+lista=threading.enumerate()
+for i in lista:
+    if "hilo" in str(i):
+        break
+else:
+    hilo=threading.Thread(target=obtener_memes, name="hilo")
+    hilo.start()
+
+
+
+
 
 # Iniciar el bot
 client.run(os.environ["token"])
